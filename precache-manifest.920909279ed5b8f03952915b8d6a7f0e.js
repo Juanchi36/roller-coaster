723d2de9e7e4b8cf0dcd5a9418270d20d58e027f
Juanchi36 <juanmartingon@hotmail.com>
2020-08-28 10:54:38 -0300

    Updates

---
+++ precache-manifest.920909279ed5b8f03952915b8d6a7f0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8128085686dfe0c9ac833c132cf82c4",
+    "url": "/roller-coaster/index.html"
+  },
+  {
+    "revision": "d9d4f14b538f2554a927",
+    "url": "/roller-coaster/static/css/main.f2785edb.chunk.css"
+  },
+  {
+    "revision": "05d964053df92ab28971",
+    "url": "/roller-coaster/static/js/2.a1db3643.chunk.js"
+  },
+  {
+    "revision": "039ea544bcc2ac534c26a97aebd87b09",
+    "url": "/roller-coaster/static/js/2.a1db3643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9d4f14b538f2554a927",
+    "url": "/roller-coaster/static/js/main.a8076c98.chunk.js"
+  },
+  {
+    "revision": "fa22f57462bf5304bee5",
+    "url": "/roller-coaster/static/js/runtime-main.46bf383e.js"
+  },
+  {
+    "revision": "7181a25efc40556b50db7f3ea89b2ed8",
+    "url": "/roller-coaster/static/media/broken.7181a25e.jpg"
+  },
+  {
+    "revision": "fdfbea05b6175a6cfd899d2a396adf93",
+    "url": "/roller-coaster/static/media/end.fdfbea05.jpg"
+  }
+]);
